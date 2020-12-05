70ff647dbe8f7015c25ccbbe1c9b37a832279da0
heaven-dev <kah@live.fi>
2020-12-05 19:31:29 +0200

    Fix to program info page.

--- app/src/main/java/sv/tv7/himlentv7/fragments/ProgramInfoFragment.java
+++ app/src/main/java/sv/tv7/himlentv7/fragments/ProgramInfoFragment.java
@@ -49,6 +49,7 @@ import static sv.tv7.himlentv7.helpers.Constants.NEGATIVE_ONE_STR;
 import static sv.tv7.himlentv7.helpers.Constants.ONE_STR;
 import static sv.tv7.himlentv7.helpers.Constants.PROGRAM_INFO_FRAGMENT;
 import static sv.tv7.himlentv7.helpers.Constants.SERIES_AND_NAME;
+import static sv.tv7.himlentv7.helpers.Constants.TWO_STR;
 import static sv.tv7.himlentv7.helpers.Constants.ZERO_STR;
 
 /**
@@ -149,6 +150,10 @@ public class ProgramInfoFragment extends Fragment {
                 else if (isVisibleInVod.equals(ONE_STR)) {
                     videoAvailable = true;
                     comingProgram = false;
+                }
+                else if (isVisibleInVod.equals(TWO_STR)) {
+                    videoAvailable = true;
+                    comingProgram = true;
                 }
                 else if (isVisibleInVod.equals(NEGATIVE_ONE_STR)) {
                     videoAvailable = false;
