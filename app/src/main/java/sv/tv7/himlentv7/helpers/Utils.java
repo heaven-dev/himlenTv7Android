84c501486c7cc3be2b924ac97a750185e61a8b70
heaven-dev <kah@live.fi>
2021-07-10 16:17:50 +0300

    Fix to favorites functionality.

--- app/src/main/java/sv/tv7/himlentv7/helpers/Utils.java
+++ app/src/main/java/sv/tv7/himlentv7/helpers/Utils.java
@@ -65,14 +65,17 @@ import static sv.tv7.himlentv7.helpers.Constants.FAVORITES_SP_DEFAULT;
 import static sv.tv7.himlentv7.helpers.Constants.FAVORITES_SP_TAG;
 import static sv.tv7.himlentv7.helpers.Constants.GUIDE_FRAGMENT;
 import static sv.tv7.himlentv7.helpers.Constants.ID;
+import static sv.tv7.himlentv7.helpers.Constants.IS_SERIES;
 import static sv.tv7.himlentv7.helpers.Constants.LOG_TAG;
 import static sv.tv7.himlentv7.helpers.Constants.NULL_VALUE;
+import static sv.tv7.himlentv7.helpers.Constants.ONE_STR;
 import static sv.tv7.himlentv7.helpers.Constants.PROGRAM_INFO_FRAGMENT;
 import static sv.tv7.himlentv7.helpers.Constants.SEARCH_FRAGMENT;
 import static sv.tv7.himlentv7.helpers.Constants.SEARCH_RESULT_FRAGMENT;
 import static sv.tv7.himlentv7.helpers.Constants.SERIES_FRAGMENT;
 import static sv.tv7.himlentv7.helpers.Constants.SERIES_INFO_FRAGMENT;
 import static sv.tv7.himlentv7.helpers.Constants.SHOW_ANIMATIONS;
+import static sv.tv7.himlentv7.helpers.Constants.SID;
 import static sv.tv7.himlentv7.helpers.Constants.TIME_STAMP_FORMAT;
 import static sv.tv7.himlentv7.helpers.Constants.TV_MAIN_FRAGMENT;
 import static sv.tv7.himlentv7.helpers.Constants.TV_PLAYER_FRAGMENT;
@@ -450,8 +453,16 @@ public abstract class Utils {
                         continue;
                     }
 
-                    if (value.equals(id)) {
-                        return i;
+                    if (key.equals(SID)) {
+                        String series = Utils.getJsonStringValue(obj, IS_SERIES);
+                        if (series != null && series.equals(ONE_STR) && value.equals(id)) {
+                            return i;
+                        }
+                    }
+                    else {
+                        if (value.equals(id)) {
+                            return i;
+                        }
                     }
                 }
             }
